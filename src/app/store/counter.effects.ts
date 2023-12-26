c39199c1e04327e12b58f97e82dca5a5f9bc9e0a
Damiano Bertuna <damianobertuna@gmail.com>
2023-12-26 17:14:58 +0100

    Saved on local storage the last value with effect

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,21 +1,24 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.actions';
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { countSelector } from './counter.selector';
 
 @Injectable()
 export class CounterEffects {
   saveCount = createEffect(
     () => this.actions.pipe(
-        ofType(increment, decrement),
-        tap((action) => {
-          console.log(action);
-          localStorage.setItem('count', action.value.toString());
-        }),
-      ),
+      ofType(increment, decrement),
+      withLatestFrom(this.store.select(countSelector)),
+      tap(([action, counter]) => {
+        console.log(action);
+        localStorage.setItem('count', counter.toString());
+      }),
+    ),
     {dispatch: false}
   );
 
-  constructor(private actions: Actions) {
+  constructor(private actions: Actions, private store: Store<{counter: number}>) {
   }
 }
